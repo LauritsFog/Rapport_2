16f4ce8b72a5fe7b06688d67cd813819d018f7fa
LauritsFog <lause.fog@gmail.com>
2022-03-09 10:44:10 +0100

    Updated looping over all days

--- Rapport_2.m
+++ Rapport_2.m
@@ -13,7 +13,7 @@ addpath(genpath('Matlab'));
 
 meanDif = abs(mean(fatPix)-mean(meatPix));
 
-[max, idx] = max(meanDif);
+[maxVal, idx] = max(meanDif);
 
 % Calculating threshold values for day 1. 
 
@@ -47,17 +47,28 @@ end
 figure(2)
 plot(errorRate);
 title('Error rate');
+%%
+
+% Combining annotations to get image of background. 
+
+background = sum(annotationIm,3);
 
 %%
 
-fatClass = zeros(514);
+% Converting from int8 to double.
+
+multiImDouble = double(multiIm);
+
+%%
+
+meatClass = zeros(514);
 
 % Classifying every meat-pixel and giving it value 1. 
 
 for i = 1:514
     for j = 1:514
-        if multiIm(i,j,idx) < meanThresholds(idx) && background(i,j) == 1
-            fatClass(i,j) = 1;
+        if multiImDouble(i,j,idx) < meanThresholds(idx) && background(i,j) == 1
+            meatClass(i,j) = 1;
         end
     end
 end
@@ -65,7 +76,7 @@ end
 %%
 
 figure(3);
-imshow(fatClass);
+imshow(meatClass);
 title('Meat-pixels classified and colored white with simple model');
 
 %%
@@ -82,18 +93,6 @@ lenFat = length(fatPix);
 pooledSigma = (1/((lenMeat-1)+(lenFat-1))).*((lenMeat-1).*meatSigma+(lenFat-1).*fatSigma);
 
 pooledSigmaInv = inv(pooledSigma);
-
-%%
-
-% Converting from int8 to double.
-
-multiImDouble = double(multiIm);
-
-%%
-
-% Combing annotations to get image of background. 
-
-background = sum(annotationIm,3);
 
 %%
 
@@ -127,12 +126,12 @@ Sdif = Smeat./Sfat;
 
 % Coloring in every meat pixel. 
 
-sClassDay1 = zeros(514);
+advClassDay1 = zeros(514);
 
 for i = 1:514
     for j = 1:514
         if Sdif(i,j) > 1 && background(i,j) == 1
-            sClassDay1(i,j) = 1;
+            advClassDay1(i,j) = 1;
         end
     end
 end
@@ -140,7 +139,7 @@ end
 %% 
 
 figure(4);
-imshow(sClassDay1);
+imshow(advClassDay1);
 title('Meat-pixels classified and colored white with advanced model');
 
 %%
@@ -149,7 +148,7 @@ errorRateDay1 = 0;
 
 for i = 1:514
     for j = 1:514
-        if sClassDay1(i,j) ~= annotationIm(i,j,3) && annotationIm(i,j,3) == 1
+        if advClassDay1(i,j) ~= annotationIm(i,j,3) && annotationIm(i,j,3) == 1
             errorRateDay1 = errorRateDay1 + 1;
         end
     end
@@ -158,35 +157,34 @@ end
 %%
 
 days = [1,6,13,20,28];
-absErrors = zeros(1,5);
+absErrorsAdv = zeros(1,5);
+absErrorsSimple = zeros(1,5);
 numPixels = zeros(1,5);
 
 for k = 1:5
     [multiIm, annotationIm] = loadMulti(strcat('multispectral_day',sprintf('%02d',days(k)),'.mat'),strcat('annotation_day',sprintf('%02d',days(k)),'.png'));
-    [fatPix, fatR, fatC] = getPix(multiIm, annotationIm(:,:,2));
-    [meatPix, meatR, meatC] = getPix(multiIm, annotationIm(:,:,3));
-
+    
     multiImDouble = double(multiIm);
 
     background = sum(annotationIm,3);
-
-    meanDif = abs(mean(fatPix)-mean(meatPix));
-
-    [max, idx] = max(meanDif);
-
-    meanThresholds = (mean(fatPix)+mean(meatPix))/2;
     
-    absErrorSimple = zeros(1,19);
+    simpleClass = zeros(514);
     
-    % Counting errors using simple model.
+    % Classifying using simple model. 1 = meat. 
     
-    for l = 1:18
-        for p = 1:756
-            if fatPix(p,l) < meanThresholds(l) || meatPix(p,l) > meanThresholds(l)
-                absErrorSimple(l) = absErrorSimple(l) + 1;
+    for i = 1:514
+        for j = 1:514
+            if multiImDouble(i,j,idx) < meanThresholds(idx) && background(i,j) == 1
+                simpleClass(i,j) = 1;
             end
         end
     end
+    
+    figure(5)
+    subplot(2,3,k);
+    imshow(simpleClass);
+    title(strcat('Day',{' '}, int2str(days(k))));
+    sgtitle('Classification using simple model');
     
     % Computing S-values for every pixel.
     
@@ -195,38 +193,45 @@ for k = 1:5
 
     for i = 1:514
         for j = 1:514
-            Sfat(i,j) = Sf_fat(multiImDouble(i,j,:));
-            Smeat(i,j) = Sf_meat(multiImDouble(i,j,:));
+            if background(i,j) == 1
+                Sfat(i,j) = Sf_fat(multiImDouble(i,j,:));
+                Smeat(i,j) = Sf_meat(multiImDouble(i,j,:));
+            end
         end
     end
 
     Sdif = Smeat./Sfat;
     
-    % Classifying pixels using advanced model. 
+    % Classifying pixels using advanced model. 1 = meat. 
 
-    sClass = zeros(514);
+    advClass = zeros(514);
 
     for i = 1:514
         for j = 1:514
             if Sdif(i,j) > 1 && background(i,j) == 1
-                sClass(i,j) = 1;
+                advClass(i,j) = 1;
             end
         end
     end
     
+    figure(6)
     subplot(2,3,k);
-    imshow(sClass);
+    imshow(advClass);
     title(strcat('Day',{' '}, int2str(days(k))));
-    
-    % Counting errors using advanced model.
+    sgtitle('Classification using advanced model');
     
     for i = 1:514
         for j = 1:514
             if annotationIm(i,j,3) == 1 || annotationIm(i,j,2) == 1
                 numPixels(k) = numPixels(k) + 1;
             end
-            if (annotationIm(i,j,3) == 1 && sClass(i,j) == 0) || (annotationIm(i,j,2) == 1 && sClass(i,j) == 1)
-                absErrors(k) = absErrors(k) + 1;
+            % Counting errors using simple model.
+            if (annotationIm(i,j,3) == 1 && simpleClass(i,j) == 0) || (annotationIm(i,j,2) == 1 && simpleClass(i,j) == 1)
+                absErrorsSimple(k) = absErrorsSimple(k) + 1;
+            end
+            % Counting errors using advanced model.
+            if (annotationIm(i,j,3) == 1 && advClass(i,j) == 0) || (annotationIm(i,j,2) == 1 && advClass(i,j) == 1)
+                absErrorsAdv(k) = absErrorsAdv(k) + 1;
             end
         end
     end
@@ -234,11 +239,15 @@ end
 
 %%
 
-errorRate = absErrors./numPixels;
+errorRateAdv = absErrorsAdv./numPixels;
+errorRateSimple = absErrorsSimple./numPixels;
 
 %%
 
-figure(5)
-bar(days,absErrors);
-figure(6)
-bar(days,errorRate);
+figure(7)
+plot(days,errorRateAdv,'b');
+hold on
+plot(days,errorRateSimple,'r');
+
+%%
+
